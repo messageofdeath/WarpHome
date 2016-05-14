a6dbc5a6caafeb7dfd164b5df7d217b127ead400
BuildTools <unconfigured@null.spigotmc.org>
2016-05-14 02:15:06 -0500

    Added a isDefault boolean | use /home for main home. Param(xyz) to Loc

--- src/me/messageofdeath/WarpHome/Utils/HomeManager/Home.java
+++ src/me/messageofdeath/WarpHome/Utils/HomeManager/Home.java
@@ -4,26 +4,29 @@ import java.util.UUID;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+
+import me.messageofdeath.WarpHome.WarpHome;
 
 public class Home {
 
-	private JavaPlugin plugin;
+	private WarpHome plugin;
 	private UUID uuid;
 	private String homeName, world;
+	private boolean isDefault;
 	private double x, y, z; 
 	private float yaw, pitch;
 	
-	public Home(JavaPlugin plugin, UUID uuid, String homeName, String world, double x, double y, double z, float yaw, float pitch) {
+	public Home(WarpHome plugin, UUID uuid, String homeName, boolean isDefault, Location loc) {
 		this.plugin = plugin;
 		this.uuid = uuid;
 		this.homeName = homeName;
-		this.world = world;
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.yaw = yaw;
-		this.pitch = pitch;
+		this.isDefault = isDefault;
+		this.world = loc.getWorld().getName();
+		this.x = loc.getX();
+		this.y = loc.getY();
+		this.z = loc.getZ();
+		this.yaw = loc.getYaw();
+		this.pitch = loc.getPitch();
 	}
 	
 	public UUID getUUID() {
@@ -32,6 +35,10 @@ public class Home {
 	
 	public String getHomeName() {
 		return this.homeName;
+	}
+	
+	public boolean isDefault() {
+		return this.isDefault;
 	}
 	
 	public Player getPlayer() {
