eacb489afd0635dc88797ae9c98bab24c07ce8e4
BuildTools <unconfigured@null.spigotmc.org>
2016-05-14 02:12:26 -0500

    Implemented the HomeManager, WarpManager, and Database into main class for execution

--- src/me/messageofdeath/WarpHome/WarpHome.java
+++ src/me/messageofdeath/WarpHome/WarpHome.java
@@ -2,16 +2,42 @@ package me.messageofdeath.WarpHome;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.messageofdeath.WarpHome.Database.Database;
+import me.messageofdeath.WarpHome.Utils.LocationParser;
 import me.messageofdeath.WarpHome.Utils.HomeManager.HomeManager;
 import me.messageofdeath.WarpHome.Utils.WarpManager.WarpManager;
 
 public class WarpHome extends JavaPlugin {
 	
+	private Database database;
 	private HomeManager homeManager;
 	private WarpManager warpManager;
 
 	@Override
 	public void onEnable() {
-		
+		LocationParser.plugin = this;
+		this.database = new Database(this);
+		this.homeManager = new HomeManager();
+		this.warpManager = new WarpManager();
+		this.database.loadHomes();
+		this.database.loadWarps();
+	}
+	
+	@Override
+	public void onDisable() {
+		this.database.saveHomes();
+		this.database.saveWarps();
+	}
+	
+	public Database getDatabaseX() {
+		return this.database;
+	}
+	
+	public HomeManager getHomeManager() {
+		return this.homeManager;
+	}
+	
+	public WarpManager getWarpManager() {
+		return this.warpManager;
 	}
 }
